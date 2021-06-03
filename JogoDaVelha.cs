644f7e2f2299c870831c4f6d2bfab5e074cdb968
enrisco <64703041+enrisco@users.noreply.github.com>
2021-06-03 12:28:44 -0300

    Uniso exercices

---
+++ JogoDaVelha.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projOldLadysGame
+{
+    class Program
+    {
+        static string[,] tab = new string[3, 3];
+        static bool p1phase = true, finish = false, draw = false;
+        static int phases = 1;
+        static string player = "";
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                int linha = 0, coluna = 0;
+                WriteTab();
+
+                if (!finish)
+                {
+                    if (p1phase)
+                    {
+                        player = "X";
+                        p1phase = false;
+                    }
+                    else
+                    {
+                        player = "O";
+                        p1phase = true;
+                    }
+                    Console.WriteLine("É a vez do {0}! Escreva a coluna e escreva a linha!", player);
+                    Console.Write("Coluna: ");
+                    coluna = Convert.ToInt32(Console.ReadLine()) - 1;
+                    Console.Write("Linha: ");
+                    linha = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                    if (tab[linha, coluna] == null)
+                        tab[linha, coluna] = player;
+
+                    if (phases >= 5)
+                    {
+                        if (phases <= 9)
+                        {
+                            if (CheckLinesAndColumns(player) || CheckDiagonal(player))
+                            {
+                                finish = true;
+                                draw = false;
+                            }
+
+                        }
+
+                        if (phases == 9)
+                        {
+                            finish = true;
+                            draw = true;
+                        }
+                    }
+
+                    phases++;
+                    Console.Clear();
+                }
+                else
+                {
+                    if (!draw)
+                        Console.WriteLine("{0} ganhou!", player);
+                    else
+                        Console.WriteLine("O jogo empatou.");
+
+                    Console.ReadKey();
+                    break;
+                }
+            }
+            while (phases != 11);
+        }
+
+        static void WriteTab()
+        {
+            Console.WriteLine("Jogo Da Velha\n");
+            for (int l = 0; l < 3; l++)
+            {
+                Console.WriteLine("\t \t|\t \t|\t \t");
+                for (int c = 0; c < 3; c++)
+                {
+                    Console.Write("\t{0}\t", tab[l, c]);
+                    if (c != 2)
+                        Console.Write("|");
+                }
+                Console.WriteLine("\n\t \t|\t \t|\t \t");
+                if (l != 2)
+                    Console.WriteLine("________________|_______________|________________");
+            }
+
+            Console.WriteLine();
+        }
+
+        static bool CheckLinesAndColumns(string player)
+        {
+            Console.WriteLine(player);
+            for (int i = 0; i < 3; i++)
+            {
+                if (tab[i, 0] == player && tab[i, 1] == player && tab[i, 2] == player)
+                    return true;
+                else if (tab[0, i] == player && tab[1, i] == player && tab[2, i] == player)
+                    return true;
+            }
+
+            return false;
+        }
+
+        static bool CheckDiagonal(string player)
+        {
+            if (tab[0, 0] == player && tab[1, 1] == player && tab[2, 2] == player)
+                return true;
+            else if (tab[0, 2] == player && tab[1, 1] == player && tab[2, 0] == player)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ TiroAoAlvo.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projTiroAoAlvo
+{
+    class Program
+    {
+        
+        static string op;
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                int alvoX = CriarPosicaoAleatoria(),
+                    alvoY = CriarPosicaoAleatoria(),
+                    jogadas = 1;
+                bool ganhou = false;
+
+                string[,] b = new string[4, 4]
+                {
+                    { " ", " ", " ", " " },
+                    { " ", " ", " ", " " },
+                    { " ", " ", " ", " " },
+                    { " ", " ", " ", " " }
+                };
+
+                do
+                {
+                    int tentX, tentY;
+                    Console.WriteLine(alvoX + ", " + alvoY);
+
+                    Console.Clear();
+                    Console.WriteLine("Tiro ao Alvo");
+
+                    CriarTabuleiro(b);
+
+                    do
+                    {
+                        tentX = ChequeSeNumeroEstaDentroDoLimite(true);
+                        tentY = ChequeSeNumeroEstaDentroDoLimite(false);
+
+                        if (ChequeSeJaEstaAssinalado(b, tentX, tentY))
+                        {
+                            Console.WriteLine("Você acertou um espaço já acertado, tente novamente.");
+                            Console.ReadLine();
+                        }
+                    } while (ChequeSeJaEstaAssinalado(b, tentX, tentY));
+
+                    if(ChequeSeAcertouAPosicao(alvoX,alvoY,tentX, tentY))
+                    {
+                        b[alvoX, alvoY] = "O";
+                        ganhou = true;
+
+                        Console.Clear();
+                        Console.WriteLine("Tiro ao Alvo");
+
+                        CriarTabuleiro(b);
+
+                        Console.WriteLine("Você ganhou! Aperte qualquer botão para continuar!");
+                    }
+                    else
+                    {
+                        b[tentX, tentY] = "X";
+                        jogadas++;
+                        
+                        if(jogadas == 4)
+                            Console.WriteLine("Você não conseguiu acertar o alvo no número de rodadas permitido.");
+                        else
+                            Console.WriteLine("Você errou! " + CriarMensagemDeDica(alvoX, alvoY, tentX, tentY));
+                        Console.WriteLine("Aperte qualquer botão para continuar.");
+                    }
+                    Console.ReadKey();
+                } while (jogadas < 5 && !ganhou);
+
+                Console.Write("Deseja continuar? (s/n): ");
+                op = Console.ReadLine().ToUpper();
+            }
+            while (op != "N");
+        }
+
+        static void CriarTabuleiro(string[,] b)
+        {
+            
+            Console.WriteLine
+            (
+                "_________________\n" +
+                "| {0} | {1} | {2} | {3} |\n" +
+                "|___|___|___|___|\n" +
+                "| {4} | {5} | {6} | {7} |\n" +
+                "|___|___|___|___|\n" +
+                "| {8} | {9} | {10} | {11} |\n" +
+                "|___|___|___|___|\n" +
+                "| {12} | {13} | {14} | {15} |\n" +
+                "|___|___|___|___|\n", b[0, 0], b[0, 1],
+                b[0, 2], b[0, 3], b[1, 0], b[1, 1], b[1, 2],
+                b[1, 3], b[2, 0], b[2, 1], b[2, 2], b[2, 3],
+                b[3, 0], b[3, 1], b[3, 2], b[3, 3]
+            );
+        }
+
+        static int CriarPosicaoAleatoria()
+        {
+            Random r = new Random();
+            return r.Next(0, 5);
+        }
+
+        static string CriarMensagemDeDica(int alvoX, int alvoY, int tentX, int tentY)
+        {
+            string m = "Dica: ";
+
+            if (tentX > alvoX)
+                m += "Tente mais para cima e ";
+            else if (tentX < alvoX)
+                m += "Tente mais para baixo e ";
+            else
+                m += "Você acertou a linha! Já a coluna, ";
+
+            if (tentY > alvoY)
+                m += "tente mais para a esquerda!";
+            else if (tentY < alvoY)
+                m += "tente mais para a direita!";
+            else
+                m += "a coluna você já acertou!";
+
+            return m;
+        }
+
+        static int ChequeSeNumeroEstaDentroDoLimite(bool linha)
+        {
+            int tmp = 5;
+
+            do
+            {
+                if (linha)
+                    Console.Write("Digite o número da linha do tiro (1 até 4): ");
+                else
+                    Console.Write("Digite o número da coluna do tiro (1 até 4): ");
+
+                tmp = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                if (tmp < 0 || tmp > 3)
+                {
+                    Console.WriteLine("Número inválido. Aperte qualquer tecla para continuar.\n");
+                    Console.ReadKey();
+                }
+
+            } while (tmp < 0 || tmp > 3);
+
+            return tmp;
+        }
+
+        static bool ChequeSeJaEstaAssinalado(string [,] b, int tentX, int tentY)
+        {
+            return b[tentX, tentY] == "X";
+        }
+
+        static bool ChequeSeAcertouAPosicao(int alvoX, int alvoY, int tentX, int tentY)
+        {
+            return tentX == alvoX && tentY == alvoY;
+        }
+    }
+}
